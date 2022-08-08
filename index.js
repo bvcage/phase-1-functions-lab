2471d2c94c46fa6a35b60f2afd4a16ecd70037c4
Bailey Cage <bailey.cage@gmail.com>
2022-08-08 17:49:08 -0400

    add distanceFromHqInFeet & distanceTravelledInFeet

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function distanceFromHqInBlocks(pickupLoc) {
     return Math.abs(42 - pickupLoc);
 }
+
+function distanceFromHqInFeet(pickupLoc) {
+    return 264 * Math.abs(42 - pickupLoc);
+}
+
+function distanceTravelledInFeet(startLoc, endLoc) {
+    return 264 * Math.abs(startLoc - endLoc);
+}
