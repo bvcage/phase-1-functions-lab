5cc47a2b9d1ab80e8e45c707fba86e99d1bbfcf6
Bailey Cage <bailey.cage@gmail.com>
2022-08-08 17:59:25 -0400

    update to pass all tests

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ function distanceFromHqInFeet(pickupLoc) {
 function distanceTravelledInFeet(startLoc, endLoc) {
     return 264 * Math.abs(startLoc - endLoc);
 }
+
+function calculatesFarePrice(startLoc, endLoc) {
+    let fare;
+    const distance = distanceTravelledInFeet(startLoc, endLoc);
+    if (distance < 400) {
+        return 0;
+    } else if (distance <= 2000) {
+        return (distance - 400) * .02;
+    } else if (distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+    return fare;
+}
